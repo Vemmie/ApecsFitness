a69902eccf4f4124927ddb3a08f911b0792d038a
Brandon Chung <b.chung8612@gmail.com>
2025-05-28 00:52:49 -0700

    Fix method names

--- components/exercises/ExercisesHeader.tsx
+++ components/exercises/ExercisesHeader.tsx
@@ -4,17 +4,15 @@ import { Appbar, withTheme } from "react-native-paper";
 type Props = {};
 
 const ExercisesHeader = ({}: Props) => {
-  const _goBack = () => console.log("Went back");
+  const goBack = () => console.log("Went back");
 
-  const _handleSearch = () => console.log("Searching");
-
-  const _handleMore = () => console.log("Shown more");
+  const handleCreate = () => console.log("Shown more");
 
   return (
     <Appbar.Header>
-      <Appbar.BackAction onPress={_goBack} />
+      <Appbar.BackAction onPress={goBack} />
       <Appbar.Content title="Exercises" />
-      <Appbar.Action icon="plus" onPress={_handleMore}></Appbar.Action>
+      <Appbar.Action icon="plus" onPress={handleCreate} />
     </Appbar.Header>
   );
 };
