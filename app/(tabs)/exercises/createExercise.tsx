de50e51f3baba54d366c21d653b725534f6afc8e
Brandon Chung <b.chung8612@gmail.com>
2025-06-20 18:13:41 -0700

    Add EquipmentSelector component

--- app/(tabs)/exercises/createExercise.tsx
+++ app/(tabs)/exercises/createExercise.tsx
@@ -1,5 +1,5 @@
 import CreateExercisesHeader from "@/components/exercises/CreateExercises/CreateExercisesHeader";
-import EquipmentChip from "@/components/exercises/CreateExercises/EquipmentChip";
+import EquipmentSelector from "@/components/exercises/CreateExercises/EquipmentSelector";
 import React from "react";
 import { StyleSheet, View } from "react-native";
 import { Text, TextInput, useTheme } from "react-native-paper";
@@ -25,78 +25,16 @@ const createExercise = () => {
           Muscles
         </Text>
 
+        <EquipmentSelector equipment={equipment} setEquipment={setEquipment} />
         <Text style={{ fontSize: 16, color: theme.colors.primary }}>
-          Equipment
+          Record Type
         </Text>
-        <View style={{ flexDirection: "column", flexGrow: 1, gap: 8 }}>
-          <View style={{ flexDirection: "row", flexGrow: 1, gap: 8 }}>
-            <EquipmentChip
-              style={styles.chip}
-              equipment="Barbell"
-              selectedEquipment={equipment}
-              setEquipment={setEquipment}
-            />
-            <EquipmentChip
-              style={styles.chip}
-              equipment="Dumbbell"
-              selectedEquipment={equipment}
-              setEquipment={setEquipment}
-            />
-          </View>
-          <View style={{ flexDirection: "row", flexGrow: 1, gap: 8 }}>
-            <EquipmentChip
-              style={styles.chip}
-              equipment="Machine"
-              selectedEquipment={equipment}
-              setEquipment={setEquipment}
-            />
-            <EquipmentChip
-              style={styles.chip}
-              equipment="Kettlebell"
-              selectedEquipment={equipment}
-              setEquipment={setEquipment}
-            />
-          </View>
-          <View style={{ flexDirection: "row", flexGrow: 1, gap: 8 }}>
-            <EquipmentChip
-              style={styles.chip}
-              equipment="Bodyweight"
-              selectedEquipment={equipment}
-              setEquipment={setEquipment}
-            />
-            <EquipmentChip
-              style={styles.chip}
-              equipment="Cable"
-              selectedEquipment={equipment}
-              setEquipment={setEquipment}
-            />
-          </View>
-          <View style={{ flexDirection: "row", flexGrow: 1, gap: 8 }}>
-            <EquipmentChip
-              style={styles.chip}
-              equipment="Cardio"
-              selectedEquipment={equipment}
-              setEquipment={setEquipment}
-            />
-            <EquipmentChip
-              style={styles.chip}
-              equipment="Other"
-              selectedEquipment={equipment}
-              setEquipment={setEquipment}
-            />
-          </View>
-        </View>
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  contents: {
-    flexGrow: 1,
-    padding: 32,
-    paddingTop: 64,
-  },
   chip: {
     width: "50%",
     flexGrow: 1,
