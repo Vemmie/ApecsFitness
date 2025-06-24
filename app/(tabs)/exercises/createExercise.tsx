38c5e60f1b6c8df06e351459368b28e1dd3b8f8b
Vemmie <94100376+Vemmie@users.noreply.github.com>
2025-06-23 21:16:45 -0700

    created a general ThemedAppHeader Comp and applied it instead of using ExceriseHeader

--- app/(tabs)/exercises/createExercise.tsx
+++ app/(tabs)/exercises/createExercise.tsx
@@ -1,13 +1,20 @@
-import CreateExercisesHeader from "@/components/exercises/CreateExercises/CreateExercisesHeader";
+import ThemedAppHeader from "@/components/ThemedAppHeader";
+import { useRouter } from "expo-router";
 import React from "react";
 import { View } from "react-native";
 import { useTheme } from "react-native-paper";
 
 const createExercise = () => {
   const theme = useTheme();
+  const router = useRouter();
+  const goBack = () => router.navigate("..");
   return (
     <View style={{ backgroundColor: theme.colors.surface, flexGrow: 1 }}>
-      <CreateExercisesHeader />
+      <ThemedAppHeader
+        title="Create Exercise" // The title for this screen
+        showBackButton={true} // Show the back button
+        onBackPress={goBack} // Pass the specific back action
+      />
     </View>
   );
 };
--- components/ThemedAppHeader.tsx
+++ components/ThemedAppHeader.tsx
@@ -2,18 +2,22 @@ import React from "react";
 import { Appbar, useTheme } from "react-native-paper";
 
 // Deinfes the props that AppHEader will accept
-type AppHeaderProps = {
+type ThemedAppHeaderProps = {
   title: string;
   showBackButton?: boolean; // optional
   onBackPress?: () => void; // optional
 };
 
 // Functional component for a reusable App Header
-const AppHeader = ({ title, showBackButton, onBackPress }: AppHeaderProps) => {
+const ThemedAppHeader = ({
+  title,
+  showBackButton,
+  onBackPress,
+}: ThemedAppHeaderProps) => {
   const theme = useTheme();
 
   return (
-    <Appbar.Header style={{ backgroundColor: theme.colors.primary }}>
+    <Appbar.Header style={{ backgroundColor: theme.colors.surfaceVariant }}>
       {/* Conditionally render back button */}
       {showBackButton && <Appbar.BackAction onPress={onBackPress} />}
 
@@ -23,4 +27,4 @@ const AppHeader = ({ title, showBackButton, onBackPress }: AppHeaderProps) => {
   );
 };
 
-export default AppHeader;
+export default ThemedAppHeader;
