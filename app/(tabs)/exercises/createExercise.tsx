cfee76c4fbd9672144cefd3a52498098319df478
Brandon Chung <b.chung8612@gmail.com>
2025-06-30 16:55:23 -0600

    Add in RecordType selector

--- app/(tabs)/exercises/createExercise.tsx
+++ app/(tabs)/exercises/createExercise.tsx
@@ -1,9 +1,10 @@
 import CreateExercisesHeader from "@/components/exercises/CreateExercises/CreateExercisesHeader";
 import EquipmentSelector from "@/components/exercises/CreateExercises/EquipmentSelector";
 import MuscleSelector from "@/components/exercises/CreateExercises/MuscleSelector";
+import RecordType from "@/constants/RecordType";
 import React from "react";
-import { StyleSheet, View, ScrollView } from "react-native";
-import { Text, TextInput, useTheme } from "react-native-paper";
+import { ScrollView, StyleSheet, View } from "react-native";
+import { RadioButton, Text, TextInput, useTheme } from "react-native-paper";
 
 const createExercise = () => {
   const theme = useTheme();
@@ -12,9 +13,12 @@ const createExercise = () => {
   const [selectedMuscles, setSelectedMuscles] = React.useState<Set<string>>(
     new Set(),
   );
+  const [recordType, setRecordType] = React.useState<RecordType>(
+    RecordType.WEIGHT_AND_REPS,
+  );
 
   return (
-    <View style={{ backgroundColor: theme.colors.surface, flexGrow: 1 }}>
+    <View style={{ backgroundColor: theme.colors.surface, flexShrink: 1 }}>
       <CreateExercisesHeader />
 
       <ScrollView>
@@ -35,9 +39,32 @@ const createExercise = () => {
             equipment={equipment}
             setEquipment={setEquipment}
           />
-          <Text style={{ fontSize: 16, color: theme.colors.primary }}>
-            Record Type
-          </Text>
+
+          <RadioButton.Group
+            onValueChange={(newValue: string) =>
+              setRecordType(newValue as RecordType)
+            }
+            value={recordType}
+          >
+            <Text style={{ fontSize: 16, color: theme.colors.primary }}>
+              Record Type
+            </Text>
+            <RadioButton.Item
+              label={RecordType.WEIGHT_AND_REPS}
+              value={RecordType.WEIGHT_AND_REPS}
+              style={styles.radioButtonItem}
+            />
+            <RadioButton.Item
+              label={RecordType.REPS}
+              value={RecordType.REPS}
+              style={styles.radioButtonItem}
+            />
+            <RadioButton.Item
+              label={RecordType.TIME}
+              value={RecordType.TIME}
+              style={styles.radioButtonItem}
+            />
+          </RadioButton.Group>
         </View>
       </ScrollView>
     </View>
@@ -48,6 +75,9 @@ const styles = StyleSheet.create({
   chip: {
     width: "50%",
     flexGrow: 1,
+  },
+  radioButtonItem: {
+    paddingHorizontal: 8,
   },
 });
 
---
+++ constants/RecordType.ts
@@ -0,0 +1,7 @@
+enum RecordType {
+  WEIGHT_AND_REPS = "Weight and Reps",
+  REPS = "Reps",
+  TIME = "Time",
+}
+
+export default RecordType;
